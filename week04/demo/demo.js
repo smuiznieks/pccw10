456860aeb55cd8a5f80c01cb32933e501d2c8dfb
Selga Muiznieks <selga.muiznieks@gmail.com>
2021-11-23 15:21:56 -0500

    code katas

---
+++ week04/demo/demo.js
@@ -0,0 +1,60 @@
+// Write a function that checks if a number is even or odd
+// Hint: % is a	modulus (division remainder)
+const data = [4, 76, 104, 119, 191, 223, 228, 317, 326, 338, 459, 474, 489, 557, 619, 647, 688, 724, 755, 956];
+
+
+
+// Write a function that draws a "square" (or maybe try a rectangle?)
+// Hint: Use HTML <pre> tag (preformatted text)
+/*
+square(4) should output:
+####
+####
+####
+####
+*/
+
+
+
+// Write a function that draws a right angle triangle
+/*
+triangle(4) should output:
+#
+##
+###
+####
+*/
+
+
+
+// Write a function that takes in a string and then prints out the longest word
+
+const paragraph = "Cronut small batch ugh bespoke, readymade helvetica mustache tattooed drinking vinegar air plant. Cronut salvia schlitz woke stumptown helvetica fashion axe pinterest microdosing slow-carb next level migas snackwave. Organic master cleanse pok pok, hammock cliche succulents tilde chillwave you probably haven't heard of them. Mixtape mumblecore selfies, thundercats tacos snackwave man braid schlitz migas salvia chicharrones.";
+
+// Write a function that calculates the total value of the contents of your wallet
+
+const values = {
+  one: 1.00,
+  five: 5.00,
+  ten: 10.00,
+  twenty: 20.00,
+  penny: 0.01,
+  nickle: 0.05,
+  dime: 0.10,
+  quarter: 0.25
+}
+
+const myWallet = [ 
+  bills = { 
+    one: 3,         // 03.00
+    five: 2,        // 10.00
+    ten: 0,         // 00.00
+    twenty: 1       // 20.00
+  },
+  coins = {
+    penny: 4,       // 00.04
+    nickle: 4,      // 00.20
+    dime: 3,        // 00.30
+    quarter: 7      // 01.75
+  }
+];
