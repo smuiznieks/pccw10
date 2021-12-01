f44a1e892ac4429eef66516de52085dd466fdea9
Selga Muiznieks <selga.muiznieks@gmail.com>
2021-11-30 21:55:58 -0500

    sorting

--- week05/README.md
+++ week05/README.md
@@ -1,1 +1,3 @@
 # Week 05
+
+`curl -o boston.js http://pollysnips.s3.amazonaws.com/bostonEmployeeSalaries.json`
--- week05/anonymous.js
+++ week05/anonymous.js
@@ -3,10 +3,12 @@ var anonymous = function() {
 }
 
 const numbers = [1, 2, 3];
+// Not an arrow function
 numbers.map(function(number) {
   console.log(`I am a number: ${number}`);
 })
 
+// Arrow function
 numbers.map(number => {
   console.log(`I am a number but fat arrow style: ${number}`)
 })
