32ca74105b9bb66352722de3b535a1d5bcf6c403
Selga Muiznieks <selga.muiznieks@gmail.com>
2021-12-14 13:04:52 -0500

    greetings exercise

---
+++ week07/greetings/greetings.js
@@ -0,0 +1,22 @@
+// This is an example of the greet() function covering requirements layed out in the Unit Testing Practice activity
+// The solution is written: export default function greet(name) {
+function greet(name) {
+  if (name === null || name === undefined) return 'Hello there!';
+  if (typeof name === typeof []) {
+    if (name.length > 0) {
+      let names = '';
+      name.forEach((elm) => {
+        names += ', ' + elm;
+      });
+      return 'Hello' + names;
+    }
+    return 'Hello there!';
+  }
+  if (name === name.toUpperCase()) {
+    return 'HELLO ' + name + '!';
+  }
+
+  return 'Hello, ' + name;
+}
+
+module.exports = greet;
