9723bccdf5ed9c0045b84fc6cd7f67c7b0ddb416
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-02-08 20:05:03 -0500

    office hours

--- week11/README.md
+++ week11/README.md
@@ -1,1 +1,61 @@
 # Week 11
+
+## Word Count
+
+```
+lorem = lorem.replaceAll(',', '').replaceAll('.', '');
+console.log(lorem);
+let data = lorem.split(' ');
+console.log(data);
+console.log(`How many words are there in this string? ${data.length}`)
+```
+
+Then update arguments: `let arguments = [data, 1, 3];`
+
+Paginate can be written as one line: `let paginate = (theData, start, pageLength) => theData.slice(start, start + pageLength);`
+
+Make sure we log the answer: `console.log(paginate(...arguments));`
+
+```
+let words = (text) => {
+  let words = text.split(' ');
+  words = words.map((item) => item.replace(',', '')).map((item) => item.replace('.', ''));
+  return words;
+}
+```
+
+```
+let wordFrequency = (words) => {
+  let count = {};
+  words.map(x => {
+    if (count[x]) {
+      count[x]++
+    } else {
+      count[x] = 1
+    }
+  })
+  return count;
+}
+```
+
+This can be rewritten like this:
+
+```
+let wordFrequency = (words) => {
+  let count = {};
+  words.map(x => count[x] ? count[x]++ : count[x] = 1)
+  return count;
+}
+```
+
+```
+<script>
+
+let numbers = [0,1,2,3,4];
+let mycopy = [...numbers];
+mycopy[2] = 9;
+console.log(mycopy);
+console.log(numbers);
+
+</script>
+```
--- week11/closures/closure.js
+++ week11/closures/closure.js
@@ -4,58 +4,69 @@ Your task is to complete the described object which uses closure to create a boo
 Additionally the bookObject will return an inner object with functions that will allow the code outside bookObject to access and modify BookObject Scoped Variables.
 */
 
-
 function defineClosure() {
-    function BookObject(name = "The Count of Monte Cristo", currentPage = 873) {
-        console.log(`\n\nCreating new Book Object with name: ${name} and currentPage: ${currentPage}`);
-        var currentPageNumber = currentPage;
-        var bookName = name;
+  function BookObject(name = "The Count of Monte Cristo", currentPage = 873) {
+    console.log(
+      `\n\nCreating new Book Object with name: ${name} and currentPage: ${currentPage}`
+    );
+    // BooKObject scoped variables
+    var currentPageNumber = currentPage;
+    var bookName = name;
 
-        // construct and return an object with the three methods defined below.
-        // Your task is to fill in these methods according to the comments in order to to allow outside functions to access           
-        // and change your BookObject variables ONLY using these gateway functions
-        // All methods have access to currentPageNumber and bookName variables, but they do not exist as regular variables outside of the bookObject
-        return {
-            "getBookTitle": function () {
-                // your code here to return the bookName
+    // construct and return an object with the three methods defined below.
+    // Your task is to fill in these methods according to the comments in order to to allow outside functions to access
+    // and change your BookObject variables ONLY using these gateway functions
+    // All methods have access to currentPageNumber and bookName variables, but they do not exist as regular variables outside of the bookObject
+    return {
+      getBookTitle: function () {
+        // your code here to return the bookName
+        return bookName;
+      },
+      turnPage: function () {
+        // your code here to increment the currentPageNumber
+        currentPageNumber++;
+      },
+      getPage: function () {
+        //your code here to return the currentPageNumber
+        return currentPageNumber;
+      },
+    };
+  }
 
-            },
-            "turnPage": function () {
-                // your code here to increment the currentPageNumber
+  var myBook = BookObject("Reckless", 28);
+  console.log(`The book name is: ${myBook.getBookTitle()}`); // closure
+  console.log(
+    `Accessing myBook's bookName variable directly: ${myBook.bookName}` // not able to access!
+  );
+  console.log(`Initial Page Number is: ${myBook.getPage()}`);
+  myBook.turnPage();
+  console.log(
+    `New Page Number after changing currentPageNumber with turnPage function: ${myBook.getPage()}`
+  );
 
-            },
-            "getPage": function () {
-                //your code here to return the currentPageNumber
+  // creating a new instance of myBook, this resets all inner variables as its completely different object and scope
+  myBook = BookObject();
+  console.log(`The book name is: ${myBook.getBookTitle()}`);
+  console.log(
+    `Accessing myBook's bookName variable directly: ${myBook.bookName}`
+  );
+  console.log(`Initial Page Number is: ${myBook.getPage()}`);
+  myBook.turnPage();
+  console.log(
+    `New Page Number after changing currentPageNumber with turnPage function: ${myBook.getPage()}`
+  );
 
-            }
-        };
-    }
-
-    var myBook = BookObject('Reckless', 28);
-    console.log(`The book name is: ${myBook.getBookTitle()}`);
-    console.log(`Accessing myBook's bookName variable directly: ${myBook.bookName}`);
-    console.log(`Initial Page Number is: ${myBook.getPage()}`);
-    myBook.turnPage();
-    console.log(`New Page Number after changing currentPageNumber with turnPage function: ${myBook.getPage()}`);
-
-    // creating a new instance of myBook, this resets all inner variables as its completely different object and scope
-    myBook = BookObject();
-    console.log(`The book name is: ${myBook.getBookTitle()}`);
-    console.log(`Accessing myBook's bookName variable directly: ${myBook.bookName}`);
-    console.log(`Initial Page Number is: ${myBook.getPage()}`);
-    myBook.turnPage();
-    console.log(`New Page Number after changing currentPageNumber with turnPage function: ${myBook.getPage()}`);
-
-    return BookObject();
+  return BookObject();
 }
 
+// console.log(currentPageNumber);
 
-// Don't change this code 
+// Don't change this code
 window.onload = () => {
-    defineClosure();
+  defineClosure();
 };
 
 //don't change this code
-if (typeof module !== 'undefined') {
-    module.exports = { defineClosure };
+if (typeof module !== "undefined") {
+  module.exports = { defineClosure };
 }
