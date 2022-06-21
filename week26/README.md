57541199772def046b02f544bcf30f009d371261
Selga P. Muiznieks <selga.muiznieks@gmail.com>
2022-06-21 19:58:51 -0400

    week 26

--- week26/README.md
+++ week26/README.md
@@ -4,6 +4,7 @@
 
 Version 9 SDK:
 https://firebase.google.com/docs/web/setup?authuser=0&hl=en
+https://firebase.google.com/docs/database/web/read-and-write#web-version-9_1
 
 React:
 https://firebase.google.com/docs/web/setup?hl=en&authuser=0
--- week26/index.js
+++ week26/index.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+// import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD12U6gGxkX7q_SDJMOVTmyjf3oHJgpn2I",
   authDomain: "emeritus-cd19d.firebaseapp.com",
@@ -8,12 +12,35 @@ const firebaseConfig = {
   appId: "1:79336576676:web:61344dce20ae202036b831",
 };
 
+// VERSION 7
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.database();
+
+// const button = document.getElementById("clickme");
+
+// button.addEventListener("click", (e) => {
+//   const logs = db.ref("logs");
+
+//   const id = Math.random().toString().replace(".", "");
+
+//   logs.child(id)
+//     .set({
+//       clickedAt: new Date().toString(),
+//       username: 'smuiznieks'
+//     }).then(() => console.log('Wrote to db!'));
+// });
+
+const app = initializeApp(firebaseConfig);
+// console.log(app);
+const db = getDatabase();
 
 const button = document.getElementById("clickme");
 
 button.addEventListener("click", (e) => {
-  
-});
+  const id = Math.random().toString().replace(".", "");
+  set(ref(db, "logs/" + id), {
+    clickedAt: new Date().toString(),
+    username: "smuiznieks",
+  }).then(() => console.log("Wrote to database!"));
+})
